e28d2678859216edc1a1ab5797a8d56bb1b4a86f
levancao798 <53284451+levancao798@users.noreply.github.com>
2021-05-12 10:26:03 +0700

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,11 @@
+#Import the public key used by the package management system
+wget --no-check-certificate -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+#Create a list file for MongoDB.
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+#Reload local package database.
+sudo apt-get update
+#Install the MongoDB packages.
+sudo apt-get install -y mongodb-org mongodb-org-server mongodb-org-shell mongodb-org-mongos mongodb-org-tools
+#Restart mongod
+systemctl restart mongod
+#Change default port mongod
